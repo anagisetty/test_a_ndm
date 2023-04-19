beb5aa9aa365b7b475a37638810b9efae529ca16
dotnet-code <@dotnet-code>
2023-04-19 15:46:43 +0530

    commit from code202304194643

---
+++ test_a_ndm/Controllers/UserController.cs
@@ -0,0 +1,59 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Test_a_NDM.Models;
+
+namespace Test_a_NDM
+{
+    public class UserController : ApiController
+    {
+        private readonly IUserRepository _repository;
+
+        public UserController(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpPost]
+        [Route("ForgotPassword")]
+        public HttpResponseMessage ForgotPassword(ForgotPasswordModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Bad Request");
+            }
+
+            bool isSuccess = _repository.ForgotPassword(model);
+
+            if (isSuccess)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Error Occurred");
+            }
+        }
+
+        [HttpPost]
+        [Route("ResetPassword")]
+        public HttpResponseMessage ResetPassword(ResetPasswordModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Bad Request");
+            }
+
+            bool isSuccess = _repository.ResetPassword(model);
+
+            if (isSuccess)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Error Occurred");
+            }
+        }
+    }
+}
