09c8022afb675e78b62bef4832f8fbf5f4eaceb1
dotnet-code <@dotnet-code>
2023-04-19 15:41:40 +0530

    commit from code202304194140

---
+++ test_a_ndm/Controllers/AuditLogController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Test_a_NDM.Management;
+
+namespace Test_a_NDM.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuditLogController : ControllerBase
+    {
+        private readonly ManagementContext _context;
+
+        public AuditLogController(ManagementContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/AuditLog
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AuditLog>>> GetAuditLogs()
+        {
+            return await _context.AuditLogs.ToListAsync();
+        }
+
+        // GET: api/AuditLog/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AuditLog>> GetAuditLog(int id)
+        {
+            var auditLog = await _context.AuditLogs.FindAsync(id);
+
+            if (auditLog == null)
+            {
+                return NotFound();
+            }
+
+            return auditLog;
+        }
+
+        // PUT: api/AuditLog/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAuditLog(int id, AuditLog auditLog)
+        {
+            if (id != auditLog.AuditLogId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(auditLog).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AuditLogExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/AuditLog
+        [HttpPost]
+        public async Task<ActionResult<AuditLog>> PostAuditLog(AuditLog auditLog)
+        {
+            _context.AuditLogs.Add(auditLog);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAuditLog", new { id = auditLog.AuditLogId }, auditLog);
+        }
+
+        // DELETE: api/AuditLog/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<AuditLog>> DeleteAuditLog(int id)
+        {
+            var auditLog = await _context.AuditLogs.FindAsync(id);
+            if (auditLog == null)
+            {
+                return NotFound();
+            }
+
+            _context.AuditLogs.Remove(auditLog);
+            await _context.SaveChangesAsync();
+
+            return auditLog;
+        }
+
+        private bool AuditLogExists(int id)
+        {
+            return _context.AuditLogs.Any(e => e.AuditLogId == id);
+        }
+    }
+}
